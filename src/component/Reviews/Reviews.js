86f634dde34a1cf603b2f2533244dbbd5c3cc55e
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-08-31 10:52:44 +0600

    header photo uploaded

--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -3,7 +3,7 @@
 const Reviews = () => {
      return (
           <>
-               <h1>This is Reviews</h1>
+               <h1 className="text-2vw text-[#91c99f] ml-32">Here are Reviews</h1>
           </>
      );
 };
