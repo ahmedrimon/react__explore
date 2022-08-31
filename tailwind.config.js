37b2feb805c7e11cd5e02ff42071a85975cefec2
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-08-31 16:50:46 +0600

    header component responsiveness complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    screen: {
-      'xs': '400px'
-    },
     extend: {
+      screens: {
+      '3xl': '1792px',
+      },
+
       fontFamily: {
         apercu: ['Apercu'],
         monarch: ['WT Monarch Nova']
       },
+      
       fontSize: {
         '2vw': '2vw',
         '1.2rem': '1.2rem'
