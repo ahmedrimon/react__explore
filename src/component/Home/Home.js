5314393c5bedc58c86c57b53c1c942fd6feac371
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-08-31 17:21:53 +0600

    Full home page done responsiveness

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,14 +1,14 @@
 // Component Uses
 import Header from "../Header/Header";
 import Reviews from "../Reviews/Reviews";
-// import ReviewsButton from "../ReviewsButton/ReviewsButton";
+import ReviewsButton from "../ReviewsButton/ReviewsButton";
 
 const Home = () => {
      return (
           <>
                <Header/>
                <Reviews/>
-               {/* <ReviewsButton/> */}
+               <ReviewsButton/>
           </>
      );
 };
